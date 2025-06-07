543347a7a96564eb5ed05773ca08e2872c26c0da
001034223 <Phuong.LUU@student.tafesa.edu.au>
2025-06-07 17:12:08 +0930

    commit changed the ticket price 1st round

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
-        const decimal mdecGeneral = 28.75m;
-        const decimal mdecBox = 62.0m;
+        const decimal mdecBalcony = 36.5m;
+        const decimal mdecGeneral = 30.75m;
+        const decimal mdecBox = 64.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
